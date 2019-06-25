4f7a96618119a066131aa8d39e6e87d6b1eabb74
Genaro <genaro_13@hotmail.es>
2019-06-25 00:27:49 -0600

    Half of video 14

--- server/models/Machine.js
+++ server/models/Machine.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
 
 const MachineSchema = new  mongoose.Schema({
-    macA: string,
+    macA: String,
     cpuLoad: Number,
-
+    freeMem: Number,
+    totalMem: Number,
+    useMem: Number,
+    memUsage: Number,
+    osType: String,
+    upTime: Number,
+    cpuModel: String,
+    numCores: Number,
+    cpuSpeed: Number
 });
 
 
--- server/socketMain.js
+++ server/socketMain.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect('http://localhost:27017/perfData', { useNewUrlParser: true})
+mongoose.connect('mongodb://localhost:27017/perfData', { useNewUrlParser: true})
+const Machine = require('./models/Machine');
 
 function socketMain(io, socket) {
     console.log('A socket connected!', socket.id);
@@ -16,9 +17,25 @@ function socketMain(io, socket) {
 
     // a machine has been connected, check to see if its new
     // if it is, add it
-    socket.on('initPerfData', (allPerformanceData) => {
-        console.log(allPerformanceData)
+    socket.on('initPerfData', (data) => {
+        // update our socket connect function scoped variables
+        macA = data.macA;
+        // now we go check mongo
+        checkAndAdd(macA);
+        console.log(data)
     })
+
+    //because we are doing db stuff, js won wait for the db
+    // so we need to make this a promise
+    function checkAndAdd( data) {
+
+        return new Promise((resolve, reject) => {
+            Machine.findOne({
+
+            })
+        })
+
+    }
     socket.on('perfData', (allPerformanceData)=> {
         console.log(allPerformanceData)
     })
