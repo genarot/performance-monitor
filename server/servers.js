216533f32cf2bb2f7163a49b02be1a7f77828bd9
Genaro Jose Tinoco Palacios <gtinoco@agssa.net>
2019-06-27 07:31:25 -0600

    First step, ioredis package installation

--- server/servers.js
+++ server/servers.js
@@ -24,6 +24,7 @@ const num_processes = require('os').cpus().length;
 // check to see if it's running -- redis-cli monitor
 const io_redis = require('socket.io-redis');
 const farmhash = require('farmhash');
+const Redis = require('ioredis');
 
 if (cluster.isMaster) {
     // This stores our workers. We need to keep them to be able to reference
@@ -88,7 +89,15 @@ if (cluster.isMaster) {
     // server is assumed to be on localhost:6379. You don't have to
     // specify them explicitly unless you want to change them.
     // redis-cli monitor
-    io.adapter(io_redis({ host: 'localhost', port: 6379 }));
+    const pubClient =  new Redis({
+        host: 'localhost',
+        port: 6379
+    });
+    const subClient =  new Redis({
+        host: 'localhost',
+        port: 6379
+    });
+    io.adapter(io_redis({ pubClient: pubClient, subClient }));
 
     // Here you might use Socket.IO middleware for authorization etc.
     // on connection, send the socket over to our module with socket stuff
